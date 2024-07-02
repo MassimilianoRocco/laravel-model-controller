75c2523787a126c2245a301a9f8cc02fc16d8f54
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-07-02 17:26:20 +0200

    Added: movies title on view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Dati da "config/data.php": {{ $home }}</h1>
-    <p>This is my paragraph content.</p>
+    @foreach ($movies as $movie)
+        <p>{{$movie->title}}</p>
+    @endforeach
 @endsection
